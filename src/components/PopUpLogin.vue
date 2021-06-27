d2f02496ca05fb335cbb2d512d7fac2e23b706a1
Zli-i <73710882+Zli-i@users.noreply.github.com>
2021-06-28 00:42:02 +0200

    Logowanie google/zwykłe/rejestracja

--- src/components/PopUpLogin.vue
+++ src/components/PopUpLogin.vue
@@ -50,8 +50,8 @@
                         </v-btn>
                     </v-col>
                     <v-col cols="2" class="offset-md-1">
-                        <v-btn dark>
-                            <v-icon right>mdi-google</v-icon>
+                        <v-btn dark v-on:click="handleClickLogin">
+                            <v-icon >mdi-google</v-icon>
                         </v-btn>
                     </v-col>
                     <v-col cols="4" class="offset-md-3">
@@ -73,12 +73,20 @@
 <script>
 import endpoint from '@/endpoint.json';
 import axios from 'axios';
+import GoogleSignInButton from 'vue-google-signin-button-directive';
+
 
   export default {
+    directives: {
+      GoogleSignInButton,
+    },
+    
     data: () => ({
       dialog: false,
       username: '',
       password: '',
+      clientId: '232184996385-jlgvk9t282s6tf0hl0maaqvu506elsi9.apps.googleusercontent.com'
+      
     }),
     methods : {
       async handleSubmit()
@@ -92,12 +100,70 @@ import axios from 'axios';
 
         localStorage.setItem('token', response.headers.authorization);
 
-        console.log(response.headers.authorization + "\nZapisano token")
+        console.log("Token" , response.headers.authorization)
 
         
-      }
+      },
+      async googleAuthPost(authCode)
+      {
+          const promise = axios.post(`${endpoint.url}/auth/google`, { code : authCode });
+
+          const headerPromise = promise.then((response) => response.headers.authorization)
+
+          return headerPromise;
+      },
+
+      handleClickLogin() {
+      this.$gAuth
+        .getAuthCode()
+        .then((authCode) => {
+          console.log("authCode", authCode);
+
+          this.googleAuthPost(authCode)
+            .then(data => {
+                localStorage.setItem('token', data);
+                console.log("Token",data);
+            }) 
+        })
+        .catch((error) => {
+          console.log("error", error)
+        });
+      },
+
+
+      /*OnGoogleAuthSuccess(idToken) {
+      console.log(idToken);
+      axios.post(`${endpoint.url}/auth/google/${idToken}`)
+          .then((response) => {
+            if (response.status === 200) {
+              console.log(this.dataToDashboard)
+              console.log(response)
+              sessionStorage.setItem('loggedIn', JSON.stringify(response.data))
+            }
+          })
+          .catch(() => {
+            this.info = 'Niepoprawne dane do logowania';
+          });
+      },
+      OnGoogleAuthFail(error) {
+        console.log(error)
+      },*/
+
     }
   }
+//272331398761-ougmqhm1r97sbjonavrhpljcbn0a37km.apps.googleusercontent.com
 
-
+//yVJN8faS6sdDusLGV5yhfJEJ
 </script>
+
+<style>
+.google-signin-button {
+  color: white;
+  background-color: red;
+  height: 50px;
+  font-size: 16px;
+  border-radius: 10px;
+  padding: 10px 20px 25px 20px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import GAuth from 'vue-google-oauth2'
 
 Vue.config.productionTip = false
+
+Vue.use(GAuth, {
+  clientId: '232184996385-jlgvk9t282s6tf0hl0maaqvu506elsi9.apps.googleusercontent.com', scope: 'email', prompt: 'consent', fetch_basic_profile: false
+})
 
 new Vue({
   router,
